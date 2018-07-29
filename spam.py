2ccf3f1a5c76a5060e4c823d5b11746e63aa3b48
riskis7 <41503041+riskis7@users.noreply.github.com>
2018-07-29 14:08:27 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,164 @@
+#!/system/xbin/bash
+ clear
+ toilet -f slant --gay "s3cd3c"
+ sleep 1
+ echo "\033[31;1m Macam-macam tool untuk spam"
+ sleep 1
+ echo "\033[33;1m Author: mod By Riskis_7"
+ sleep 1
+ echo "\033[32;1m ★contack: #Riskis_7"
+ sleep 1
+ echo "\033[34;1m ★ANONYMOUS CYBER TEAM"
+ sleep 1
+ echo "\033[36;1m Spam Yang Tersedia :"
+ echo "1. Telkomsel"
+ echo "2. Tokopedia"
+ echo "3. Matahari Mall"
+ echo "4. Bukalapak"
+ echo "5. Shoppe"
+ echo "6. Axis"
+ echo "7. Tokoh Bagus"
+ echo "8. Indosat"
+ echo "9. Bank Bri"
+ echo "10. PHD"
+ echo "11. Jd.Id"
+ echo "12. Email Bomber"
+ echo "0. Keluar"
+ echo "\033[30;1m81. Informasi Tool Ini"
+ echo "\033[33;1m Pilih Angka:"
+ read mrrm
+ if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+ then
+clear
+echo "\033[34;1m"
+figlet "T-Sel"
+php 1.php
+fi
+ 
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Tokopedia"
+php 2.php
+fi
+ 
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[32;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+ 
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+echo "\033[35;1m"
+figlet "Bukalapak"
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+echo "\033[37;1m"
+figlet "Shoppe"
+php 5.php
+fi
+
+if [ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+echo "\033[38;1m"
+figlet "Axis"
+php 6.php
+fi
+
+if [ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+echo "\033[39;1m"
+figlet "Tokoh Bagus"
+php 7.php
+fi
+
+if [ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Indosat"
+php 8.php
+fi
+
+if [ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+echo "\033[40;1m"
+figlet "Bank Bri"
+php 9.php
+fi
+
+if [ $mrrm = 10 ] || [ $mrrm = 10 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[42;1m"
+php 10.php
+fi
+ 
+if
+[ $mrrm = 11 ] || [ $mrrm = 11 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[43;1m"
+php 11.php
+fi
+ 
+if
+[ $mrrm = 12 ] || [ $mrrm = 12 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 12.py
+fi
+ 
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "BctSpam"
+
+echo "\033[31;1mNama tools: BctSpam"
+sleep 1
+echo "\033[33;1mKarya: RISKIS7 (V7 By RISKIS7)"
+sleep 1
+echo "\033[32;1mVersi: v7"
+sleep 1
+echo "\033[36;1mTeam: ★BENGKULU CYBER TEAM★"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut KunjUngi IG:Anonymous7"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "BARRET CYBER TEAM"
+echo "BENGKULU CYBER TEAM"
+sleep 1
+echo "Riski7"
+echo "\033[30;1m tunggu 10 detik"
+sleep 10
+sh BCTSPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m silahkan keluar"
+sleep 1
+echo "\033[32;1m sampai jumpa kembali :)"
+sleep 1
+fi
+ 
+
